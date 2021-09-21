ed6926da91417fdfd4bf0df675596667cefeafe4
DESKTOP-8R0UOBE\Desktop <donypramujo@gmail.com>
2021-09-21 22:23:20 +0700

    no message

--- plugins/notes/migrate.sql
+++ plugins/notes/migrate.sql
@@ -6,6 +6,14 @@ ALTER TABLE ap_tender_tenders ADD invite_date timestamp DEFAULT NULL NULL;
 ALTER TABLE ap_tender_tenders ADD invite_hour_start timestamp DEFAULT NULL NULL;
 ALTER TABLE ap_tender_tenders ADD invite_hour_end timestamp DEFAULT NULL NULL;
 
+ALTER TABLE ap_tender_tenders_tenants ADD invite_name varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
+ALTER TABLE ap_tender_tenders_tenants ADD invite_description varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
+ALTER TABLE ap_tender_tenders_tenants ADD invite_location varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
+ALTER TABLE ap_tender_tenders_tenants ADD invite_pic_phone_number varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL NULL;
+ALTER TABLE ap_tender_tenders_tenants ADD invite_date timestamp DEFAULT NULL NULL;
+ALTER TABLE ap_tender_tenders_tenants ADD invite_hour_start timestamp DEFAULT NULL NULL;
+ALTER TABLE ap_tender_tenders_tenants ADD invite_hour_end timestamp DEFAULT NULL NULL;
+
 
 ALTER TABLE ap_tender_tenders_tenants ADD is_payment_rfp tinyint(1) DEFAULT 0 NOT NULL;
 
