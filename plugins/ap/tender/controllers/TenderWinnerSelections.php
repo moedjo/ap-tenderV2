90b1b55e117ac488114edda8544d34dc62d01f1d
M Rezza Ramadhan <mrezza.ramadhan@gmail.com>
2021-09-26 23:26:08 +0700

    update winner selection

--- plugins/ap/tender/controllers/TenderWinnerSelections.php
+++ plugins/ap/tender/controllers/TenderWinnerSelections.php
@@ -2,35 +2,37 @@
 
 namespace Ap\Tender\Controllers;
 
+use Ap\Tender\Models\TenderTenant;
 use Backend\Classes\Controller;
 use BackendMenu;
-use Ap\Tender\Models\TenderWinnerSelection;
 
 class TenderWinnerSelections extends Controller
 {
     public $implement = [
         'Backend\Behaviors\ListController',
         'Backend\Behaviors\FormController',
-        'Backend\Behaviors\RelationController'
     ];
 
     public $listConfig = 'config_list.yaml';
     public $formConfig = 'config_form.yaml';
-    public $relationConfig = 'config_relation.yaml';
 
     public $requiredPermissions = [
-        'ap_tender_is_admin_tender'
+        'ap_tender_access_tenders'
     ];
 
     public function __construct()
     {
         parent::__construct();
-        $this->pageTitle = "Pemilihan Pemenang";
         BackendMenu::setContext('Ap.Tender', 'tender', 'tender-winnerselection');
     }
 
-    public function formBeforeSave($model)
+    public function formAfterSave($model)
     {
-        $model->is_candidate_winner = 1;
+        $selected_winner = post('TenderWinnerSelection[tender_winner_selection]');
+        foreach ($selected_winner as $winner) {
+            $tenderTenant = TenderTenant::find($winner);
+            $tenderTenant->is_candidate_winner = 1;
+            $tenderTenant->save();
+        }
     }
 }
--- plugins/ap/tender/models/TenderWinnerSelection.php
+++ plugins/ap/tender/models/TenderWinnerSelection.php
@@ -3,59 +3,49 @@
 namespace Ap\Tender\Models;
 
 use Model;
+use Ap\Tender\Models\TenderTenant;
 
-/**
- * Model
- */
 class TenderWinnerSelection extends Model
 {
-    /**
-     * @var string The database table used by the model.
-     */
+    use \October\Rain\Database\Traits\Purgeable;
+
     public $table = 'ap_tender_tenders';
 
-    /**
-     * @var array Validation rules
-     */ 
-    public $rules = [];
-
     public $belongsTo = [
+        'tenant' => [
+            'Ap\Tender\Models\Tenant',
+            'key' => 'tenant_id'
+        ],
         'business_field' => [
             'Ap\Tender\Models\BusinessField',
             'key' => 'business_field_id',
         ],
-
         'airport' => [
             'Ap\Tender\Models\Airport',
             'key' => 'airport_id',
         ],
     ];
 
-    public $hasMany = [];
-
-    public $morphMany = [];
-
-    public $belongsToMany = [];
-
-    public $attachOne = [];
-
-    public $attachMany = [];
-
-    protected $jsonable = [
-        'tender_winner_selection'
+    public $belongsToMany = [
+        'tender_tenants' => [
+            'Ap\Tender\Models\TenderTenant',
+            'table' => 'ap_tender_tenders_tenants',
+            'key'      => 'tender_id',
+        ],
     ];
+
+    protected $purgeable = ['tender_winner_selection'];
 
     public function getTenderWinnerSelectionOptions()
     {
         $tenantTenders = TenderTenant::all();
         $result = [];
-        foreach ($tenantTenders as $tenant) {
-            if ($this->id == $tenant->tender_id) {
-                $result[$tenant->id] = [$tenant->invite_name];
+        foreach ($tenantTenders as $tenantTender) {
+            if ($this->id == $tenantTender->tender_id && $tenantTender->is_candidate_winner === 0 && $tenantTender->is_winner === 0) {
+                $result[$tenantTender->id] = [$tenantTender->tenant->name];
             }
         }
 
         return $result;
     }
-
 }
