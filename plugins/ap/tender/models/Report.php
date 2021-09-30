f3e2440869f8e63057b3cc76b5178983ce15fe20
M Rezza Ramadhan <mrezza.ramadhan@gmail.com>
2021-10-01 00:12:32 +0700

    report kualifikasi

---
+++ plugins/ap/tender/models/Report.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Ap\Tender\Models;
+
+use Model;
+
+/**
+ * Model
+ */
+class Report extends Model
+{
+    use \October\Rain\Database\Traits\Purgeable;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'ap_tender_tenders';
+
+    public $belongsTo = [
+        'business_field' => [
+            'Ap\Tender\Models\BusinessField',
+            'key' => 'business_field_id',
+        ],
+    ];
+
+    public $hasMany = [
+        'tender_tenants' => [
+            'Ap\Tender\Models\TenderTenant',
+            'table' => 'ap_tender_tenders_tenants',
+            'key'      => 'tender_id',
+        ],
+    ];
+
+    public $belongsToMany = [
+            'tenant_invites' => [
+                'Ap\Tender\Models\Tenant',
+                'table' => 'ap_tender_tenders_tenant_invites',
+                'key'      => 'tender_id',
+                'otherKey' => 'tenant_id',
+            ]
+    ];
+
+    protected $purgeable = ['report_description'];
+}
