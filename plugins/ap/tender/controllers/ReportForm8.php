c52196fec691a64f0e340964252240a9a8c165c3
M Rezza Ramadhan <mrezza.ramadhan@gmail.com>
2021-10-03 01:14:24 +0700

    report hasil penilaian envelope II

---
+++ plugins/ap/tender/controllers/ReportForm8.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Ap\Tender\Controllers;
+
+use Backend\Classes\Controller;
+use BackendMenu;
+use Ap\Tender\Models\TenderTenant;
+use Renatio\DynamicPDF\Classes\PDF;
+
+class ReportForm8 extends Controller
+{
+    public $implement = [
+        'Backend\Behaviors\ListController',
+        'Backend\Behaviors\FormController',
+    ];
+
+    public $listConfig = 'config_list.yaml';
+    public $formConfig = 'config_form.yaml';
+
+    public $requiredPermissions = [
+        'ap_tender_access_tenders', 'ap_tender_is_finance', 'ap_tender_is_legal', 'ap_tender_is_commercial'
+    ];
+
+    public function __construct()
+    {
+        parent::__construct();
+        BackendMenu::setContext('Ap.Tender', 'reporting', 'report-form8');
+    }
+
+    public function print($model)
+    {
+        $tender = TenderTenant::findOrFail($model);
+
+        $data['tender_tenant'] = $tender;
+        $day = date('D');
+        $dayList = array(
+            'Sun' => 'Minggu',
+            'Mon' => 'Senin',
+            'Tue' => 'Selasa',
+            'Wed' => 'Rabu',
+            'Thu' => 'Kamis',
+            'Fri' => 'Jumat',
+            'Sat' => 'Sabtu'
+        );
+        $data['hari'] = $dayList[$day];
+
+        return PDF::loadTemplate('ap.tender::pdf.report-form8', $data)->stream();
+    }
+
+    public function onSendEmailForm()
+    {
+        $this->addCss('/modules/backend/formwidgets/fileupload/assets/css/fileupload.css');
+        $config = $this->makeConfig('$/ap/tender/models/report/send_email_fields.yaml');
+
+        $config->model = new \Ap\Tender\Models\TenderTenant;
+
+        $widget = $this->makeWidget('Backend\Widgets\Form', $config);
+
+        $pdf = $this->savePdf($this->params[0]);
+
+        $this->vars['recordId'] = $this->params[0];
+        $this->vars['widget'] = $widget;
+        $this->vars['file_name'] = $pdf['pdf_file_name'];
+        $this->vars['file'] = url('storage/app/uploads') . '/' . $pdf['pdf_file_name'];
+
+        return $this->makePartial('sendEmail');
+    }
+
+    protected function savePdf($model)
+    {
+        $tender_tenant = TenderTenant::find($model);
+
+        $data['tender_tenant'] = $tender_tenant;
+        $day = date('D');
+        $dayList = array(
+            'Sun' => 'Minggu',
+            'Mon' => 'Senin',
+            'Tue' => 'Selasa',
+            'Wed' => 'Rabu',
+            'Thu' => 'Kamis',
+            'Fri' => 'Jumat',
+            'Sat' => 'Sabtu'
+        );
+        $data['hari'] = $dayList[$day];
+
+        $storagePath =  storage_path('app/uploads/');
+        $pdf_file_name =  $name = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '_', "BERITA ACARA NEGOSIASI FINAL"))) . '_' . date('H') . '.pdf';
+        $pdf_file_name_directory =  $storagePath . $pdf_file_name;
+
+        PDF::loadTemplate('ap.tender::pdf.report-form8', $data)->save($pdf_file_name_directory);
+
+        return [
+            'pdf_file_name' => $pdf_file_name
+        ];
+    }
+
+    public function onSendEmail()
+    {
+        $this->asExtension('FormController')->update_onSave(post('record_id'));
+
+        $data = post();
+
+        Mail::send('ap.tender::mail.tenant-invite2', $data, function ($message) {
+            $message->to('mrezza.ramadhan@gmail.com', 'John Doe');
+            $message->cc('mrezza.ramadhan@gmail.com', 'John Doe');
+            $message->bcc('mrezza.ramadhan@gmail.com', 'John Doe');
+            $message->subject('Penetapan Pemenang');
+            $message->attach(storage_path('app/uploads/') . post('file_name'));
+        });
+
+        return $this->listRefresh();
+    }
+}
