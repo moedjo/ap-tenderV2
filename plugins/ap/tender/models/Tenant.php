4e1e4d9fb0972deee9c3c2142634db6ed6ad12cd
DESKTOP-8R0UOBE\Desktop <donypramujo@gmail.com>
2021-10-25 22:37:19 +0700

    update shortlisted tenant

--- plugins/ap/tender/models/Tenant.php
+++ plugins/ap/tender/models/Tenant.php
@@ -185,7 +185,7 @@ class Tenant extends Model
     {
         $business_field = $tender->business_field;
         return $query
-            // ->where('status', 'short_listed')
+            ->where('status', 'short_listed')
             ->whereHas('business_fields', function ($query) use ($business_field) {
                 $query->where('id', $business_field->id);
             });
