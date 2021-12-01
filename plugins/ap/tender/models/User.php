fe087c4f1d425e9f6903330d58d47e387f6f9001
DESKTOP-8R0UOBE\Desktop <donypramujo@gmail.com>
2021-12-01 14:44:34 +0700

    takeout email

--- plugins/ap/tender/models/User.php
+++ plugins/ap/tender/models/User.php
@@ -14,7 +14,8 @@ class User extends \Backend\Models\User
 
 
     public $rules = [
-        'email' => 'required|between:6,255|email|unique:backend_users',
+        // 'email' => 'required|between:6,255|email|unique:backend_users',
+        'email' => 'required|between:6,255|email',
         'login' => [
             'required', 'between:2,255', 'unique:backend_users',
             'regex:/^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$/'
